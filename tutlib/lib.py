0fe107287e7cbda6446864b811076f95650d4d03
martintb <tyler.martin@nist.gov>
2023-11-16 03:28:34 +0000

    Updating lib.py

--- tutlib/lib.py
+++ tutlib/lib.py
@@ -617,3 +617,99 @@ def plot_ternary_scatter(data, components, labels=None, set_axes_labels=True, te
             ax.set(**labels)
             ax.grid('on', color='black')
         return artists
+
+import tensorflow as tf
+import gpflow
+
+class GaussianProcess:
+    def __init__(self,dataset,kernel=None):
+        self.reset_GP(dataset,kernel)
+        self.iter_monitor = lambda x: None
+        self.final_monitor = lambda x: None
+        
+    def construct_data(self):
+
+        if 'labels_ordinal' not in self.dataset:
+            raise ValueError('Must have labels_ordinal variable in Dataset before making GP!')
+
+            
+        labels = self.dataset['labels_ordinal'].values
+        if len(labels.shape)==1:
+            labels = labels[:,np.newaxis]
+            
+        domain = self.transform_domain()
+        
+        data = (domain,labels)
+        return data
+        
+    def transform_domain(self,components=None):
+      if components is None:
+        components = self.dataset.attrs['components']
+      if not (len(self.dataset.attrs['components'])==3):   
+          raise ValueError("Ternary domain transform specified but len(components)!=3") 
+      comp = self.dataset[components].to_array('component').transpose(...,'component')
+      domain = ternary_to_xy(comp.values)
+      return domain
+            
+    def reset_GP(self,dataset,kernel=None):
+        self.dataset = dataset
+        self.n_classes = dataset.attrs['n_phases']
+
+        data = self.construct_data()
+            
+        if kernel is None:
+            kernel = gpflow.kernels.Matern32(variance=0.1,lengthscales=0.1) 
+            
+        invlink = gpflow.likelihoods.RobustMax(self.n_classes)  
+        likelihood = gpflow.likelihoods.MultiClass(self.n_classes, invlink=invlink)  
+        self.model = gpflow.models.VGP(
+            data=data, 
+            kernel=kernel, 
+            likelihood=likelihood, 
+            num_latent_gps=self.n_classes
+        ) 
+        self.loss = self.model.training_loss_closure(compile=True)
+        self.trainable_variables = self.model.trainable_variables
+        self.optimizer = tf.optimizers.Adam(learning_rate=0.001)
+        
+    def reset_monitoring(self,log_dir='test/',iter_period=1):
+        model_task = ModelToTensorBoard(log_dir, self.model,keywords_to_monitor=['*'])
+        lml_task   = ScalarToTensorBoard(log_dir, lambda: self.loss(), "Training Loss")
+        
+        fast_tasks = MonitorTaskGroup([model_task,lml_task],period=iter_period)
+        self.iter_monitor = Monitor(fast_tasks)
+        
+        image_task = ImageToTensorBoard(
+            log_dir, 
+            self.plot, 
+            "Mean/Variance",
+            fig_kw=dict(figsize=(18,6)),
+            subplots_kw=dict(nrows=1,ncols=3)
+        )
+        slow_tasks = MonitorTaskGroup(image_task) 
+        self.final_monitor = Monitor(slow_tasks)
+
+    def optimize(self,N,final_monitor_step=None,progress_bar=False):
+        if progress_bar:
+            for i in tqdm.tqdm(tf.range(N),total=N):
+                self._step(i)
+        else:
+            for i in tf.range(N):
+                self._step(i)
+            
+        if final_monitor_step is None:
+            final_monitor_step = i
+        self.final_monitor(final_monitor_step)
+            
+    @tf.function
+    def _step(self,i):
+        self.optimizer.minimize(self.loss,self.trainable_variables) 
+        self.iter_monitor(i)
+    
+    def predict(self,components):
+        domain = self.transform_domain(components=components)
+        self.y = self.model.predict_y(domain)
+        self.y_mean = self.y[0].numpy() 
+        self.y_var = self.y[1].numpy() 
+        return {'mean':self.y_mean,'var':self.y_var}
+
