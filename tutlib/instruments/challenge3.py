da8bf4ed34c6398a04d885c97b42e1c6bda44216
Tyler Martin <tyler.martin@nist.gov>
2024-04-22 13:41:54 -0400

    [bugfix] fixes array type annotation

--- tutlib/instruments/challenge3.py
+++ tutlib/instruments/challenge3.py
@@ -10,7 +10,7 @@ from tutlib.VirtualInstrument import VirtualSAS
 
 
 def sphere_radius(a: float, b: float, c: float, a_c: float = 0.47, b_c: float = 0.37,
-                  c_c: float = 0.16) -> array.array[float] | float:
+                  c_c: float = 0.16) -> array.array | float:
     """
     Parameters
     ----------
